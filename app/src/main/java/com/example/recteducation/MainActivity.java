d79ad6537d58634c5eade30cfbcb78af614122c2
Raf <Raf_0707@mail.ru>
2021-11-04 13:50:50 +0300

    zero coefficients commit_3

--- app/src/main/java/com/example/recteducation/MainActivity.java
+++ app/src/main/java/com/example/recteducation/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.recteducation;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Вызывается при нажатии пользователем на кнопку Решить
      */
+    @SuppressLint("SetTextI18n")
     public void run(View view) {
 
         double a = Double.parseDouble(((EditText)
@@ -30,20 +32,53 @@ public class MainActivity extends AppCompatActivity {
         double c = Double.parseDouble(((EditText)
                 findViewById(R.id.c)).getText().toString());
         TextView result = findViewById(R.id.res);
-        double D = b * b - 4 * a * c;
         double x1, x2;
-        if (D == 0){
-            x1 = -b / (2 * a);
+        if (a == 0) {
+            x1 = -c / b;
             result.setText("x = " + x1);
         }
-        else if (D > 0){
-            x1 = (-b + Math.sqrt(D))/2*a;
-            x2 = (-b - Math.sqrt(D))/2*a;
+        else if (c == 0) {
+            x1 = 0;
+            x2 = -b / a;
             result.setText(String.format(Locale.US,"x1 =  %f\n\nx2 = %f", x1, x2));
         }
+        else if (b == 0) {
+            if ((-c / a) > 0) {
+                x1 = Math.sqrt(-c / a);
+                x2 = (-1) * (Math.sqrt(-c / a));
+                result.setText(String.format(Locale.US, "x1 =  %f\n\nx2 = %f", x1, x2));
+            }
+            else if ((-c / a) < 0) {
+                result.setText("Уравнение имеет 2 комплексных корня");
+            }
+        }
+        else if (a == 0 && c == 0) {
+            x1 = 0;
+            result.setText("x = " + x1);
+        }
+        else if (a == 0 && b == 0) {
+            result.setText("Нет решений");
+        }
+        else if (a == 0 && b == 0 && c == 0) {
+            result.setText("Нет решений");
+        }
+        else if (b == 0 && c == 0) {
+            x1 = 0;
+            result.setText("x = " + x1);
+        }
+        else if (a > 0 && b > 0 && c > 0) {
+            double D = b * b - 4 * a * c;
 
-        else if (D < 0){
-            result.setText("Уравнение имеет 2 комплексных корня");
+            if (D == 0) {
+                x1 = -b / (2 * a);
+                result.setText("x = " + x1);
+            } else if (D > 0) {
+                x1 = (-b + Math.sqrt(D)) / 2 * a;
+                x2 = (-b - Math.sqrt(D)) / 2 * a;
+                result.setText(String.format(Locale.US, "x1 =  %f\n\nx2 = %f", x1, x2));
+            } else if (D < 0) {
+                result.setText("Уравнение имеет 2 комплексных корня");
+            }
         }
     }
 }
